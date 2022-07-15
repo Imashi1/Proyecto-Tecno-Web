1fbb3636ef598b4b975bacbf0ac2c8bc755190fc
araeri <aracely.leona1@gmail.com>
2022-07-15 00:37:30 -0400

    formulario del agregar tienda

--- resources/php/vendor/vendor_register_form.php
+++ resources/php/vendor/vendor_register_form.php
@@ -1,70 +1,63 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
     <meta charset="utf-8">
-    <title>Display a map on a webpage</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>UTARICO Agregar Tiendas</title>
     <meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no">
     <link href="https://api.mapbox.com/mapbox-gl-js/v2.9.1/mapbox-gl.css" rel="stylesheet">
     <script src="https://api.mapbox.com/mapbox-gl-js/v2.9.1/mapbox-gl.js"></script>
+    <!-- Conexion con Bootstrap css -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
-    <style>
-        body {
-            margin: 0;
-            padding: 0;
-        }
-        
-        #map {
-            position: absolute;
-            top: 0;
-            bottom: 0;
-            width: 100%;
-            height: 80%;
-        }
-                /* Clase principal (hijo de contenedor) */
-        .principal{
-            /* margin-top: 50px; */
-            background-color: white;
-            padding: 20px;
-            /* Siempre tomara el mismo ancho que el contenedor */
-            width: 100%;
-        }
-    </style>
+    <!-- Conexion con la hoja de estilo estilo-->
+    <link type="text/css" rel="stylesheet" href="../../css/estilo.css">
 </head>
 
 <body>
     <?php
-    //Obtenemos el id del administrador
-    $id = $_GET['id'];
+        //Obtenemos el id del administrador
+        session_start();
+        include("../conexion.php");
+        $id = $_GET['id'];
+        $ID= $_SESSION["id"];
     ?>
     <!-- SECCION NAVEGACIÓN-->
-    <nav class="navbar navbar-dark bg-primary navbar-expand-md col-12">
-        <div class="container">
-            <a href="../admin/admin_main_dashboard.php" class="navbar-brand">
-                <strong>UTARICO</strong>
-            </a>
-
-            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#menu-principal" aria-controls="menu-principal" aria-expanded="false" aria-label="Desplegar menú de navegación">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="menu-principal">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item"><a href="estadistica.html" class="nav-link">Estadísticas</a></li>
+    <!--Header-->
+    <header>
+        <nav class="navbar navbar-dark bg-primary navbar-expand-md col-12">
+            <div class="container">      
+                <a href="../admin/admin_main_dashboard.php" class="navbar-brand">
+                  <strong>UTARICO</strong>
+                </a>
+                  <button type="button" class="navbar-toggler" data-toggle="collapse"
+                  data-target="#menu-principal" aria-controls="menu-principal" aria-expanded="false"
+                  aria-label="Desplegar menú de navegación">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+          
+                <div class="collapse navbar-collapse" id="menu-principal">
+                  <ul class="navbar-nav ml-auto">
+                    <li class="nav-item"><a href="../admin/admin_vendor_dashboard.php" class="nav-link">Ver Tiendas</a></li>
+                    <li class="nav-item"><a href="../vendor/vendor_register_form.php?id='<?php echo $ID; ?>'" class="nav-link">Agregar Tienda</a></li>
                     <li class="nav-item"><a href="../admin/admin_logout.php" class="nav-link">Cerrar Sesión</a></li>
-                </ul>
+                  </ul>
+                </div>
             </div>
-        </div>
-    </nav>
+        </nav>
+    </header>
     <!-- Divimos la sección en dos partes -->
-    <section class="principal">
-        <div class="row">
+    <section class="container mt-3">
+        <div class="row h-100">
             <!--La primera que contiene el formulario para agregar una nueva tienda-->
             <div class="col-12 col-md-6">
-
                 <!--Action que dirige los datos insertados al registro-->
                 <form action="vendor_register_trigger.php" method="POST" enctype="multipart/form-data" class="bg-light my-3 p-3 border rounded">
-
+                    <div class="text-center">
+                        <h2>Agregar Tienda</h2>
+                        <p class="hint-text">Agrega un nombre y una descripcion breve</p>
+                    </div>
                     <div class="form-group">
                         <label for="nombreTienda">Nombre</label>
                         <input type="text" class="form-control" name="nombreTienda" id="nombreTienda" placeholder="Nombre de la Tienda" required>
@@ -72,7 +65,7 @@
 
                     <div class="form-group">
                         <label for="descripcion">Descripción</label>
-                        <input type="textarea" class="form-control" name="descripcion" id="descripcion" placeholder="Descripción" required>
+                        <textarea class="form-control" name="descripcion" id="descripcion" placeholder="Descripción" required></textarea>
                     </div>
 
                     <div class="form-group">
@@ -89,11 +82,10 @@
                         <label for="longitud">Longitud</label>
                         <input type="text" class="form-control" name="longitud" id="longitud" readonly required>
                     </div>
-
+                     
                     <!--Input extra escondido para obtener el id y con el, poder utilizarlo en editar.php-->
                     <input type="hidden" class="form-control" value="<?php echo $id; ?>" name="id">
-                    
-
+                                      
                     <button type="submit" name="accion" value="editar" class="btn btn-success">Agregar</button>
                 </form>
             </div>
